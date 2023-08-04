23ffad0d4abf4f694077d48075472411a778c5fe
teddywlq <teddy.wlq@gmail.com>
2023-08-04 16:16:23 +0800

    We need to get the value of display ctl reg before we program it. Otherwise, some default value will be lost.

--- ddk768/ddk768_mode.c
+++ ddk768/ddk768_mode.c
@@ -895,8 +895,10 @@ pll_value_t *pPLL               /* Pre-calculated values for the PLL */
                                    DISPLAY_CTRL,
                                    HDMI_SELECT);    
 
+	ulTmpValue = peekRegisterDWord(DISPLAY_CTRL+offset);
+
     /* Set control register value */
-    ulTmpValue =       
+    ulTmpValue |=       
         (pModeParam->vertical_sync_polarity == POS
         ? FIELD_SET(0, DISPLAY_CTRL, VSYNC_PHASE, ACTIVE_HIGH)
         : FIELD_SET(0, DISPLAY_CTRL, VSYNC_PHASE, ACTIVE_LOW))
